9031198d0f4e9c300af388d71d8aae16be714f46
Fernando Mendes <fernando.mendes91@outlook.com>
2025-10-14 21:12:10 -0300

    Implementacao functions

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,53 @@
+public class Functions {
+
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    //Somente 1 nível de abstração
+    //A
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.save(user);
+    }  
+
+    public void saveUser(User user){};
+
+    public User saveUser(User user){
+        if(user.isAdmin){
+            user.setRole(user);
+        }
+        return userRepository.save(user);
+    }
+
+    public Environment getEnvironment(){
+        return environmentService.getEnvironment(getVersion());
+    }
+    public Release getRelease(){
+        return releaseService.getRelease(getVersion());
+    }
+    private String getVersion(){ 
+        return versionService.getVersion();
+    }
+
+}
+
+    //  No side effects
+    //A
+    public User saveUser(User user){
+        if(user.isAdmin){
+        user.setRole(user);
+      }
+        return userRepository.save(user);
+      }
+  
+      //B
+      public User saveUser(User user){
+          return userRepository.save(user);
+      }
+      public void setRole(User user){
+          user.setRole(user);     
+      }
